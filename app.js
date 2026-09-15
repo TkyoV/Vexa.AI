61f7992bb30885f52673367d9b4b12bf13ca5fed
TkyoV <tokyovoid.host@gmail.com>
2026-09-15 21:29:52 +0530

    Update API endpoint in localReply function

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function addMessage(text, who) {
 }
 
 async function localReply(text) {
-  const response = await fetch("http://localhost:3000/api/chat", {
+  const response = await fetch("http://localhost:3000/chat", {
     method: "POST",
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify({message: text})
